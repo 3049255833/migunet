eb38a5843af7948ca8a0c6e73153c3b13f1822cf
eric <eric.wbl@foxmail.com>
2017-10-11 20:02:03 +0800

    Code formatting

--- config/index.js
+++ config/index.js
@@ -176,27 +176,27 @@ module.exports = {
 
             '/productCenter/getBossInfo': {
                 /*获取业务状态列表*/
-                target: proxy_service_ljy,
+                target: proxy_service,
                 changeOrigin: true
             },
             '/productCenter/deleteBossInfo': {
                 /*删除业务代码*/
-                target: proxy_service_ljy,
+                target: proxy_service,
                 changeOrigin: true
             },
             '/productCenter/addBossInfo': {
                 /*添加业务代码*/
-                target: proxy_service_ljy,
+                target: proxy_service,
                 changeOrigin: true
             },
             '/productCenter/updateBossInfo': {
                 /*编辑业务代码*/
-                target: proxy_service_ljy,
+                target: proxy_service,
                 changeOrigin: true
             },
             '/productCenter/batchAddBossInfo': {
                 /*批量导入业务代码*/
-                target: proxy_service_ljy,
+                target: proxy_service,
                 changeOrigin: true
             },
 
